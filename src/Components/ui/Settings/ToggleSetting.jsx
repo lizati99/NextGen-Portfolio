3696425d1a169436d4f6473adf59b6e34a4e4dac
lizati99 <mohammedlizati99@gmail.com>
2025-03-12 23:31:50 +0200

    Added full-screen toggle functionality using the Fullscreen

--- src/Components/ui/Settings/ToggleSetting.jsx
+++ src/Components/ui/Settings/ToggleSetting.jsx
@@ -1,40 +1,62 @@
-import { faExpand, faGear, faPause, faPauseCircle, faPlay, faPlayCircle } from "@fortawesome/free-solid-svg-icons";
+import { faExpand, faGear, faPauseCircle, faPlayCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classes from "./ToggleSetting.module.css";
 import LanguageToggle from "../Languages/LanguageToggle";
 import ColorToggle from "../ColorToggle/ColorToggle";
 import { useState } from "react";
 
-export default function ToggleSetting({setIsStopped}) {
+export default function ToggleSetting({ setIsStopped }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
     setIsOpen((prevIsOpen) => !prevIsOpen);
   };
 
+  // دالة لتفعيل وإيقاف وضع الشاشة الكاملة
+  const toggleFullScreen = () => {
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen().catch((err) => {
+        console.log(`Error attempting to enable full-screen mode: ${err.message}`);
+      });
+    } else {
+      document.exitFullscreen();
+    }
+  };
+
   return (
     <div className={classes.setting_container}>
       <div className={classes.toggle_settings} onClick={handleClick}>
-        <FontAwesomeIcon icon={faGear} size="lg"/>
+        <FontAwesomeIcon icon={faGear} size="lg" />
       </div>
       <div
         className={`${classes.settings} ${isOpen ? classes.show : classes.hide}`}
         id="settings"
       >
         <h3 className={classes.setting_title}>Full Screen :</h3>
-        <div className={classes.setting_item}>
-          <FontAwesomeIcon icon={faExpand} size="lg"/>
+        <div className={classes.setting_item} onClick={toggleFullScreen}>
+          <FontAwesomeIcon icon={faExpand} size="lg" />
         </div>
-        
+
         <h3 className={classes.setting_title}>Colors & Theme :</h3>
         <ColorToggle />
-        
+
         <h3 className={classes.setting_title}>Languages :</h3>
         <LanguageToggle />
+
         <h3 className={classes.setting_title}>Dynamic Introduction :</h3>
         <div className={classes.setting_item}>
-          <FontAwesomeIcon icon={faPauseCircle} className={`${classes.control} ${classes.active}`} size="lg" onClick={() => setIsStopped(true)}/>
-          <FontAwesomeIcon icon={faPlayCircle} className={`${classes.control} ${classes.active}`} size="lg" onClick={() => setIsStopped(false)}/>
+          <FontAwesomeIcon
+            icon={faPauseCircle}
+            className={`${classes.control} ${classes.active}`}
+            size="lg"
+            onClick={() => setIsStopped(true)}
+          />
+          <FontAwesomeIcon
+            icon={faPlayCircle}
+            className={`${classes.control} ${classes.active}`}
+            size="lg"
+            onClick={() => setIsStopped(false)}
+          />
         </div>
       </div>
     </div>
